d0fe27438b2c44affb03778c82849b1e2a9e6a6c
Jan Ole Suhr <ole.suhr@rocket-es.com>
2019-05-18 11:09:11 +0200

    Publish the config! :-)

--- src/PackageServiceProvider.php
+++ src/PackageServiceProvider.php
@@ -8,10 +8,12 @@ class PackageServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-		$this->commands([\janole\Laravel\Dockerize\Console\Commands\ContainerStartup::class]);
+        $this->publishes([__DIR__.'/../config/dockerize.php' => config_path('dockerize.php')]);
 
-		$this->commands([\janole\Laravel\Dockerize\Console\Commands\DockerBuild::class]);
+        $this->commands([\janole\Laravel\Dockerize\Console\Commands\ContainerStartup::class]);
 
-		$this->commands([\janole\Laravel\Dockerize\Console\Commands\DockerCompose::class]);
+        $this->commands([\janole\Laravel\Dockerize\Console\Commands\DockerBuild::class]);
+
+        $this->commands([\janole\Laravel\Dockerize\Console\Commands\DockerCompose::class]);
     }
 }
