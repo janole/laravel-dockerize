59eae9a595a4e521407b6147f48d3b5eea3f87f3
Jan Ole Suhr <ole.suhr@rocket-es.com>
2019-06-02 00:09:52 +0200

    feat: Add custom ENV variables to the (local) docker-compose file (format: DOCKERIZE_COMPOSE_ENV_$NAME=$VALUE)

--- src/Console/Commands/DockerCompose.php
+++ src/Console/Commands/DockerCompose.php
@@ -81,6 +81,14 @@ class DockerCompose extends Command
             $env[$key] = $val;
         }
 
+        foreach ($_ENV as $key => $val)
+        {
+            if (strpos($key, "DOCKERIZE_COMPOSE_ENV_") === 0)
+            {
+                $env[substr($key, strlen("DOCKERIZE_COMPOSE_ENV_"))] = $val;
+            }
+        }
+
         $app["environment"] = $env;
 
         //
