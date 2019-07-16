7b6d0ca4974f819eec039c23b71232404397c6d0
Jan Ole Suhr <ole@mobileways.de>
2019-07-16 14:36:54 +0200

    Remove config (now everything is in env())

--- src/PackageServiceProvider.php
+++ src/PackageServiceProvider.php
@@ -6,14 +6,14 @@ use Illuminate\Support\ServiceProvider;
 
 class PackageServiceProvider extends ServiceProvider
 {
-	public function register()
-	{
-		$this->mergeConfigFrom(__DIR__.'/../config/dockerize.php', 'dockerize');
-	}
+    public function register()
+    {
+        // $this->mergeConfigFrom(__DIR__.'/../config/dockerize.php', 'dockerize');
+    }
 
     public function boot()
     {
-        $this->publishes([__DIR__.'/../config/dockerize.php' => config_path('dockerize.php')]);
+        // $this->publishes([__DIR__.'/../config/dockerize.php' => config_path('dockerize.php')]);
         $this->publishes([__DIR__.'/../docker/dockerignore' => base_path('.dockerignore')]);
 
         $this->commands([\janole\Laravel\Dockerize\Console\Commands\ContainerStartup::class]);
